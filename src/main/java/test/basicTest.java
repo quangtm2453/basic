c28b6f67f9059ca7479043ce3164c3e7df369753
quangtm2453 <minhquang147346@gmail.com>
2025-04-22 12:02:13 +0700

    lll

--- src/main/java/test/basicTest.java
+++ src/main/java/test/basicTest.java
@@ -8,7 +8,7 @@ public class basicTest {
 		int validNumber = 0;
 		int sum = 0;
 		int average;
-		int a = 112233445566;
+		int a = 11223344556677;
 		
 		while((value[i]!=-999)&& inputNumber<100) {
 			inputNumber++;
